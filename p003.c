582d203c6f7c1d6920574c9582c7f8ae1cda61b3
Jevarom <ni.moravej@gmail.com>
2017-11-08 01:19:38 -0800

    Resolved issue with p003 computing correct results

--- p003.c
+++ p003.c
@@ -3,21 +3,26 @@
 int main()
 {
 
-int n = 2, num = 0,c = 0;
+long long int n = 2;
+long long int num = 0,c = 0;
 float check = 0.0;
 
 
 printf ("Enter a number: ");
-scanf ("%d", &num);
+scanf ("%lld", &num);
 
 //do
-  for (n; num != n; n++){
-  if (num % n == 0) {
-    printf ("%d \n", n);
+  for (n; num != 1; n++){
+  if ((int)num % (int)n == 0) {
+    printf ("%lld \n", n);
     num = num/n;
+    printf ("Remain: %lld \n", num);
+    n = 1;
+    //printf ("Keep %d\n", num);
   }
-  else if (num % n != 0) {
-
+  else if (num < n) {
+    printf ("Checking: %lld \n", n);
+    n = 2;
   }
 
 } //while (check != (float)1.0);
